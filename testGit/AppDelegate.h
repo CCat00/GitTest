2056a07c751b9fa12a48f2efec76e8d803247b2c
韩威 <hw1627@163.com>
2016-05-17 16:23:11 +0800

    Initial Commit

---
+++ testGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by 韩威 on 16/5/17.
+//  Copyright © 2016年 韩威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by 韩威 on 16/5/17.
+//  Copyright © 2016年 韩威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
