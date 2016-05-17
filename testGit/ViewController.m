5708a91e78cc690b81077db8c7089a2b952cd381
韩威 <hw1627@163.com>
2016-05-17 16:47:52 +0800

    last branch test

--- testGit/ViewController.m
+++ testGit/ViewController.m
@@ -19,6 +19,8 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     // hello word! test Git
+    
+    // hehe Last Branch!
 }
 
 - (void)didReceiveMemoryWarning {
